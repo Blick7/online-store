ca13a784de20165cad82bd8fa29b851bd8e8d5ee
Blick <45530302+Blick7@users.noreply.github.com>
2022-07-22 18:50:50 +0300

    refactor: delete on update and move to Control class

--- src/application/components/rangeSlider.ts
+++ src/application/components/rangeSlider.ts
@@ -1,5 +1,7 @@
 import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
+import { filters } from '../filters';
+import Filter from '../view/Filter';
 
 const sliderInStock = document.getElementById('sliderInStock') as noUiSlider.target;
 const sliderPrice = document.getElementById('sliderPrice') as noUiSlider.target;
@@ -15,36 +17,12 @@ export default function createRangeSlider(): void {
     });
 
     noUiSlider.create(sliderPrice, {
-        start: [0, 200],
+        start: [0, 1100],
         connect: true,
         range: {
             min: 1,
-            max: 200,
+            max: 1100,
         },
         tooltips: true,
-    });
-
-    sliderInStock.noUiSlider?.on('update', () => {
-        if (!sliderInStock.noUiSlider?.get()) return;
-
-        const data = sliderInStock.noUiSlider.get() as string[];
-        const qLeft = document.querySelector<HTMLDivElement>('.in-stock__quantity-left');
-        const qRight = document.querySelector<HTMLDivElement>('.in-stock__quantity-right');
-
-        if (!qLeft || !qRight) return;
-        qLeft.textContent = data[0].split('.')[0];
-        qRight.textContent = data[1].split('.')[0];
-    });
-
-    sliderPrice.noUiSlider?.on('update', () => {
-        if (!sliderPrice.noUiSlider?.get()) return;
-
-        const data = sliderPrice.noUiSlider.get() as string[];
-        const qLeft = document.querySelector<HTMLDivElement>('.price__quantity-left');
-        const qRight = document.querySelector<HTMLDivElement>('.price__quantity-right');
-
-        if (!qLeft || !qRight) return;
-        qLeft.textContent = data[0].split('.')[0];
-        qRight.textContent = data[1].split('.')[0];
     });
 }
